6c6d7d16623436996e931030455a810d6e430f03
ahmedgyar <ahmedyassergyar@gmail.com>
2024-02-18 01:49:20 +0200

    ss

--- sort.h
+++ sort.h
@@ -1,9 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
-#include <stdio.h>
 #include <stddef.h>
-
-
+#include <stdlib.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -13,16 +11,25 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
-
+void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-void bubble_sort(int *array, size_t size);
-
-
+listint_t *swap_node(listint_t *node, listint_t **list);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
 #endif
