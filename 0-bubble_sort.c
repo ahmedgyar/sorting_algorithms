29975dbcdd21dbc89de14a539859deb13f70b26d
ahmedgyar <ahmedyassergyar@gmail.com>
2024-02-18 02:09:38 +0200

    ss

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,22 +7,19 @@
  **/
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int temp;
+	size_t i, j, n;
 
-	i = 0;
-	while (i < size )
+	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size - i; j++)
+		for (j = 1; j < size - i; j++)
 		{
-			if (array[j] > array[j + 1])
+			if (array[j - 1] > array[j])
 			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
+				n = array[j];
+				array[j] = array[j - 1];
+				array[j - 1] = n;
 				print_array(array, size);
 			}
 		}
-		i++;
 	}
 }
